f0e00bb8453f5c15d6075fca2cad3c653ff9d9f0
Armando Capriles <armandotechno12@gmail.com>
2023-11-03 23:14:03 -0400

    Refactor de la conexion con clases

--- database/MySQLi/Connection.php
+++ database/MySQLi/Connection.php
@@ -1,31 +1,68 @@
 <?php
 
-// Esto debe de estar en una variable de entorno
-$server   = "localhost";
-$database = "finanzas_personales";
-$username = "root";
-$password = "password";
+// Crear el namespace para que composer reconozca la clase al precargar.
+namespace Database\MySQLi;
 
-// Esta es la forma de conectarse procedural
-// $mysqli = mysqli_connect($server, $username, $password, $database);
+// Crear la clase para la autocarga de archivos de composer
+// Se suele colocar el nombre del archivo a la clase
+class Connection
+{
+    private static $instance;
+
+    private $connection;
+
+    private function __construct()
+    {
+        $this->make_connection();
+    }
+
+    public static function getInstance()
+    {
+
+        // Si esta propiedad no es una misma instancia de ella misma, la volvemos una instancia 
+        // self hace referencia al nombre de Connection, pero para referirse a la misma clase se usa el self
+        if (!self::$instance instanceof self) {
+            self::$instance = new self();
+
+            return self::$instance;
+        }
+    }
+
+    public function get_database_instance()
+    {
+        return $this->connection;
+    }
+
+    private function make_connection()
+    {
+        // Esto debe de estar en una variable de entorno
+        $server   = "localhost";
+        $database = "finanzas_personales";
+        $username = "root";
+        $password = "password";
+
+        // Esta es la forma de conectarse procedural
+        // $mysqli = mysqli_connect($server, $username, $password, $database);
 
 
-// Esta es la forma de conectarse orientada a objetos
-$mysqli = new mysqli($server, $username, $password, $database);
+        // Esta es la forma de conectarse orientada a objetos
+        $mysqli = new \mysqli($server, $username, $password, $database);
 
 
-// Comprobar conexión de manera procedural
-// if (!$mysqli) {
-// die("Falló la conexión: " . mysqli_connect_error());
-// }
+        // Comprobar conexión de manera procedural
+        // if (!$mysqli) {
+        // die("Falló la conexión: " . mysqli_connect_error());
+        // }
 
-// Comprobar conexión de manera orientada a objetos
-if ($mysqli->connect_errno) {
-    die("Falló la conexión: {$mysqli->connect_error}");
+        // Comprobar conexión de manera orientada a objetos
+        if ($mysqli->connect_errno) {
+            die("Falló la conexión: {$mysqli->connect_error}");
+        }
+
+        // Esto es para usar cualquier caracter en las consultas
+        $setnames = $mysqli->prepare("SET NAMES 'utf8'");
+        $setnames->execute();
+
+        $this->connection = $mysqli;
+    }
 }
-
-// Esto es para usar cualquier caracter en las consultas
-$setnames = $mysqli->prepare("SET NAMES 'utf8'");
-$setnames->execute();
-
-var_dump($setnames);
